46501a64be9eb0796410cefe9797cb68c6a0b206
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2019-12-06 09:31:13 -0500

    api token added, authorization added to post man. implement first get handler

--- src/app.js
+++ src/app.js
@@ -3,9 +3,12 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
+const winston = require('winston')
+const uuid = require('uuid/v4')
 const { NODE_ENV } = require('./config')
 
 const app = express()
+const store = require('../store')
 
 const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
@@ -14,10 +17,34 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
+app.use(express.json())
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({filename: 'info.log'})
+  ]
+})
 
 app.get('/', (req,res) => {
     res.send('Hello, world!')
 })
+
+app.use(function validateBearerToken(req, res, next) {
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    logger.error(`Unauthorized request to path: ${req.path}`)
+    return res.status(401).json({ error: 'Unauthorized request' })
+  }
+  // move to the next middleware
+  next()
+})
+
+
 
 app.use(function errorHandler(error, req, res, next) {
        let response
